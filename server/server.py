e4fc9191f27d223ae5bed82720f733c7e47cc3e9
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-06-07 21:50:13 +0200

    Basic server which stores submitted URLs in a Flask database.

--- server/server.py
+++ server/server.py
@@ -1,13 +1,32 @@
+import json
+from datetime import datetime
 from flask import Flask
-from flask import request
+from flask import request, abort
+from pymongo import Connection, json_util
 
 app = Flask(__name__)
 
+
+connection = Connection('localhost', 27017)
+db = connection.descarty
+
+
 @app.route("/", methods=("GET", "POST",))
 def index():
-	print request.values.get('url'),  request.values.get('star')
-	import time
-	time.sleep(0.2)
-	return '{"starred": true}'
+	url = request.values.get('url')
+	if not url:
+		abort(404)
+		
+	# Save the request
+	document = {'url': url}
+	if request.values.get('star', False):
+		document['starred'] = True		
+	db.pages.update({'url': url}, 
+					{'$set': document, '$push': {'visits': datetime.utcnow()}}, True)
+
+	# Get current state		
+	document = db.pages.find_one({'url': url})
+	return json.dumps({'url': document['url'], 'starred': document.get('starred', False)})
+
 
 app.run(debug=True)
